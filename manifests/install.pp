bf917320c4b174b6f3d4df15f826f6c3fedf3f79
Evgeniy Shishkin <e_shishkin@wargaming.net>
2015-06-29 18:14:15 +0300

    Fix systemd bullshit

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,4 +5,19 @@ class pgbouncer::install inherits pgbouncer {
   package { $pgbouncer::package_name:
     ensure => present,
   }
+
+
+  if "${::operatingsystem}${::operatingsystemmajrelease}" == 'CentOS7' {
+    include systemd
+    file { '/etc/systemd/system/pgbouncer.service':
+      ensure  => file,
+      source  => 'puppet:///modules/pgbouncer/pgbouncer.service',
+      require => Package[$pgbouncer::package_name],
+      mode    => '0644',
+    }
+    ~>
+    Exec['systemctl-daemon-reload']
+    ~>
+    Service[$pgbouncer::service_name]
+  }
 }
